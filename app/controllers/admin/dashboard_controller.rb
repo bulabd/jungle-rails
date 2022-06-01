e10be4844b79b2ad1649a2fca49735f7396b12ab
bulabd <bulabd@outlook.com>
2022-06-01 19:02:05 +0000

    Implement admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @product_count = Product.count.to_i
+    @category_count = Category.count.to_i
   end
 end
