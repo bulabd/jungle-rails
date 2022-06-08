7a3be9ebb2145c661ecb22d54be38b69b3ccb409
bulabd <bulabd@outlook.com>
2022-06-08 13:10:42 +0000

    Delete commented out code

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    # resources :categories, only: [:index, :create]
     resources :categories, except: [:edit, :update, :show]
   end
 
